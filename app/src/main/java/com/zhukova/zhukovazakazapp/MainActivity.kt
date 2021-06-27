ca5ec54ad4452cba4f6f5f2d652a62aa868eb25f
schicout <dgzhukov@gmail.com>
2021-06-27 20:45:37 +0300

    Поправила радиокнопки.

--- app/src/main/java/com/zhukova/zhukovazakazapp/MainActivity.kt
+++ app/src/main/java/com/zhukova/zhukovazakazapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.zhukova.zhukovazakazapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ScrollView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.korzina_zakaza)
+        setContentView(R.layout.korzina_zakaza);
+
 
     }
 }
